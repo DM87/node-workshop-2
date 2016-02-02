2df20bd0a759b05239883398550668be6a2a0fa9
dm87 <danieljamesmcauley@gmail.com>
2016-02-02 20:23:52 +0000

    dan ex3 complete

---
+++ cps.js
@@ -0,0 +1,22 @@
+
+
+
+function firstLetter(sentence, func) {
+    return func(sentence[0]);
+    }
+
+//console.log(firstLetter('Whatever'));
+
+
+
+function lastLetter(sentence, func) {
+   return func(sentence[sentence.length - 1]);
+}
+
+//lastLetter('whatever', function(x){console.log(x)});
+
+//EX3
+function getFirstAndLast(sentence, funct){
+    return firstLetter(sentence, funct) + lastLetter(sentence, funct);
+}
+getFirstAndLast("hello", function(newStr) { console.log(newStr); }); // should output "ho"
